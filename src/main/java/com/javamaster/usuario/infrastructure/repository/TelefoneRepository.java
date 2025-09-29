f57603124727db55aa0c76e84ae4c8fe3c28ffbb
Lohan Rosa <lohan.rosa@smilatam.net>
2025-09-29 11:18:09 -0300

    Adicionadas classes de security

---
+++ src/main/java/com/javamaster/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package com.javamaster.usuario.infrastructure.repository;
+
+import com.javamaster.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+
+}
